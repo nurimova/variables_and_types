5963b315b33e0b09f6119dadf0790cdf859d9462
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-11-29 18:29:35 +0200

    Update begin04.py

--- begin04.py
+++ begin04.py
@@ -1,5 +1,5 @@
-"a" o'zgaruvchisiga 12 (butun son) qiymatini bering.
+# "a" o'zgaruvchisiga 12 (butun son) qiymatini bering.
 
-"b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
+# "b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
 
-"a" va "b" bo'linmasini chop eting.
+# "a" va "b" bo'linmasini chop eting.
