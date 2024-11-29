04c594ee10d74adba077940a7f1ae40b35849c25
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-11-29 18:30:38 +0200

    Update begin01.py

--- begin01.py
+++ begin01.py
@@ -1,5 +1,5 @@
-"num_one" o'zgaruvchisiga 5 (butun son) qiymatini bering.
+# "num_one" o'zgaruvchisiga 5 (butun son) qiymatini bering.
 
-"num_two" o'zgaruvchisiga 4 (butun son) qiymatini bering.
+# "num_two" o'zgaruvchisiga 4 (butun son) qiymatini bering.
 
-"num_one" va "num_two" yig'indisini chop eting.
+# "num_one" va "num_two" yig'indisini chop eting.
