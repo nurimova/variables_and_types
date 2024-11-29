7c0055a9a24806402192740393842ab7fd26f459
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-11-29 18:29:16 +0200

    Update begin05.py

--- begin05.py
+++ begin05.py
@@ -1,7 +1,7 @@
-"a" o'zgaruvchisiga 7 (butun son) qiymatini bering.
+# "a" o'zgaruvchisiga 7 (butun son) qiymatini bering.
 
-"b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
+# "b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
 
-"c" o'zgaruvchisiga 5 (butun son) qiymatini bering.
+# "c" o'zgaruvchisiga 5 (butun son) qiymatini bering.
 
-Uch o'zgaruvchining "a", "b", "c" yig'indisini chop eting.
+# Uch o'zgaruvchining "a", "b", "c" yig'indisini chop eting.
