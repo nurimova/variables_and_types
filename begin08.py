8a4c31f00988cfdc540e1fd8ad598116c2728ad7
nurimova <n.b.sitora@gmail.com>
2024-12-05 12:09:00 +0500

    begin-8

--- begin08.py
+++ begin08.py
@@ -1,9 +1,10 @@
 # "a" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+a=5
 # "b" o'zgaruvchisiga 4 (butun son) qiymatini bering.
-
+b=4
 # "c" o'zgaruvchisiga 8 (butun son) qiymatini bering.
-
+c=8
 # Quyidagi ifodani hisoblang: 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin08';
-
+k=a*b/c
 # Natijani chop eting.
+print(k)
