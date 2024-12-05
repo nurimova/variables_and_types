ab9703037eb157ebee914afe2f2d8988206529f6
nurimova <n.b.sitora@gmail.com>
2024-12-05 12:03:49 +0500

    begin-6

--- begin06.py
+++ begin06.py
@@ -1,9 +1,10 @@
 # "a" o'zgaruvchisiga 24 (butun son) qiymatini bering.
-
+a=24
 # "b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
-
+b=3
 # "c" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+c=5
 # Quyidagi ifodani hisoblang va "k" o'zgaruvchisiga bering: 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin06';
-
+k=a-b+c
 # "k" qiymatini chop eting.
+print(k)
