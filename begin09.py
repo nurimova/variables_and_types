7ef98f7af93434176d23e430643ec4887a185a90
nurimova <n.b.sitora@gmail.com>
2024-12-05 12:13:06 +0500

    begin-10

--- begin09.py
+++ begin09.py
@@ -1,11 +1,12 @@
 # "a" o'zgaruvchisiga 8 (butun son) qiymatini bering.
-
+a=8
 # "b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
-
+b=3
 # "c" o'zgaruvchisiga 2 (butun son) qiymatini bering.
-
+c=2
 # "d" o'zgaruvchisiga 4 (butun son) qiymatini bering.
-
+d=4
 # Quyidagi ifodani hisoblang va "k" o'zgaruvchisiga bering: 'https://github.com/xakimovallamurod/variables_and_types/blob/main/begin09.py'
-
+k=a+b-c*d
 # "k" qiymatini chop eting.
+print(k)
--- begin10.py
+++ begin10.py
@@ -1,11 +1,12 @@
 # "a" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+a=5
 # "b" o'zgaruvchisiga 4 (butun son) qiymatini bering.
-
+b=4
 # "c" o'zgaruvchisiga 2 (butun son) qiymatini bering.
-
+c=2
 # "d" o'zgaruvchisiga 8 (butun son) qiymatini bering.
-
+d=8
 # Quyidagi ifodani hisoblang va "k" o'zgaruvchisiga bering: 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin10' 
-
+k=2*(a*b-c*d)
 # "k" qiymatini chop eting.
+print(k)
