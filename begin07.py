580b53c3375ffa68bae6870935789c05d5b92f57
nurimova <n.b.sitora@gmail.com>
2024-12-05 12:07:05 +0500

    begin-7

--- begin07.py
+++ begin07.py
@@ -1,7 +1,9 @@
 # "a" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+a=5
 # "b" o'zgaruvchisiga 4 (butun son) qiymatini bering.
-
+b=4
 # "c" o'zgaruvchisiga 8 (butun son) qiymatini bering.
-
+c=8
 # Quyidagi ifodani hisoblang: 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin07';
+k=(2*a/b)*c
+print (k)
