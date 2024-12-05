f5b8d215f3c25d027375c0e949f10d9ebb7c5f93
nurimova <n.b.sitora@gmail.com>
2024-12-05 11:54:59 +0500

    begin-5

--- begin05.py
+++ begin05.py
@@ -1,7 +1,9 @@
 # "a" o'zgaruvchisiga 7 (butun son) qiymatini bering.
-
+a=7
 # "b" o'zgaruvchisiga 3 (butun son) qiymatini bering.
-
+b=3
 # "c" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+c=5
 # Uch o'zgaruvchining "a", "b", "c" yig'indisini chop eting.
+d=a+b+c
+print(d)
